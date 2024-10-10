19ae61401f059a4cc9018976431a5c99195f1bde
Seyed Sadra Mousavi <120718390+SMousavi7@users.noreply.github.com>
2024-10-10 09:55:53 -0700

    Create Main.java

---
+++ Static Binding/Main.java
@@ -0,0 +1,13 @@
+class car {
+    void Driving()
+    {
+        System.out.println("car is going...");
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+        car c = new car();
+        c.Driving();
+    }
+}
